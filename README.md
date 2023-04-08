42a01f20d2849ae6c7a7d35dedceab733075d274
Yukihiro1123 <murasima1123@gmail.com>
2023-04-08 21:13:26 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# riverpod_todo
+
+Flutter ライブラリ
+
+flutter pub add freezed_annotation json_annotation hooks_riverpod flutter_hooks cached_network_image go_router
+
+flutter pub add firebase_auth firebase_core cloud_firestore firebase_storage firebase_analytics
+
+flutter pub add --dev build_runner freezed json_serializable
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+void main() {
+  runApp(const ProviderScope(child: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      title: 'Flutter Demo',
+      theme: ThemeData.dark(),
+    );
+  }
+}
