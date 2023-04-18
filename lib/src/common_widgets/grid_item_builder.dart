ede76eb560fc6d729c056484b2c7fc5dd076722b
Yukihiro1123 <murasima1123@gmail.com>
2023-04-18 16:17:43 +0900

    shimmer修正

--- lib/src/common_widgets/grid_item_builder.dart
+++ lib/src/common_widgets/grid_item_builder.dart
@@ -48,7 +48,7 @@ class GridItemsBuilder<T> extends StatelessWidget {
             ),
             itemCount: 10,
             itemBuilder: (context, index) {
-              return const ShimmerImage(height: 10);
+              return const Card(child: ShimmerImage(height: 10));
             });
       },
       error: (error, error2) {
